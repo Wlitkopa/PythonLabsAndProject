f540be2464f7253801bc5916e0e2b43fa4c12d74
Wlitkopa <przemek@student.agh.edu.pl>
2022-10-07 20:47:08 +0200

    Zrobione zadanie 3

---
+++ lab1/skrypt.py
@@ -0,0 +1,33 @@
+import sys
+import math
+
+
+def is_prime(num):
+    if num == 1:
+        return None
+    for i in range(2, math.ceil((num / 2) + 1)):
+        if num % i == 0:
+            return None
+    return num
+
+
+def primes(list):
+    for i in range(len(list)):
+        try:
+            a = int(list[i])
+
+            if is_prime(a) != None:
+                print(is_prime(a))
+
+        except ValueError:
+            pass
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        sys.argv.pop(0)
+        primes(sys.argv)
+    else:
+        print("Podaj argumenty w command line")
+else:
+    pass
